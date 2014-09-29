b7943d7cd1e99b2948f9f06683ac22c4e8c351ee
Guido Zuccarelli <zucca_17k@hotmail.com>
2014-09-29 19:05:35 -0300

    initial commit

---
+++ trabajo/src/main/java/BBDD2/trabajo/App.java
@@ -0,0 +1,13 @@
+package BBDD2.trabajo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
