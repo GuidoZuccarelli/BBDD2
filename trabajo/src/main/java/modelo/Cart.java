70870b87e6b94481b14d6b127fbea15194a80b91
Sebastian <seba.i.pierini@gmail.com>
2014-10-03 17:54:02 -0300

    nuevos cambios

--- trabajo/src/main/java/modelo/Cart.java
+++ trabajo/src/main/java/modelo/Cart.java
@@ -4,6 +4,38 @@ import java.util.Timer;
 
 public class Cart {
 	private long id;
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Timer getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(Timer creationTime) {
+		this.creationTime = creationTime;
+	}
+
 	private int userId;
 	private String token;
 	private Timer creationTime;
