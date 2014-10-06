c498daa454ee1bd4de2fc229230d900473a2db11
Guido Zuccarelli <zucca_17k@hotmail.com>
2014-10-06 19:23:11 -0300

    singleton finished

---
+++ trabajo/src/main/java/BBDD2/trabajo/hibernate/Hbutil.java
@@ -0,0 +1,24 @@
+package BBDD2.trabajo.hibernate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class Hbutil {
+
+    private static SessionFactory sessionFactory;
+
+    private static SessionFactory configureSessionFactory() throws HibernateException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        StandardServiceRegistryBuilder builder= new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());     
+        sessionFactory = configuration.buildSessionFactory(builder.build());
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return configureSessionFactory();
+
+    }
+}
--- trabajo/src/main/java/BBDD2/trabajo/singleton/MasterContainer.java
+++ trabajo/src/main/java/BBDD2/trabajo/singleton/MasterContainer.java
@@ -1,16 +1,40 @@
 package BBDD2.trabajo.singleton;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import BBDD2.trabajo.hibernate.Hbutil;
 import BBDD2.trabajo.model.Master;
 
 public class MasterContainer {
 
 	private Master master;
 	private static MasterContainer instance;
-	
+
+	public MasterContainer(Master master) {
+		super();
+		this.master = master;
+	}
+
 	public static MasterContainer getInstance(){
 		if (instance == null){
-			
+			Session session = Hbutil.getSessionFactory().openSession();
+			Transaction tx = null;
+			try{
+				tx = session.beginTransaction();
+				long id = 1;
+				instance = new MasterContainer((Master)session.get(Master.class, id));
+				tx.commit();
+			}catch (Exception e){
+				e.printStackTrace();
+				if (tx != null)
+					tx.rollback();
+				session.close();
+			}
 		}
 		return instance;
+	}
+	
+	public Master getMaster(){
+		return this.master;
 	}
 }
